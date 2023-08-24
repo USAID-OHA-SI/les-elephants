347fbb2654ffd88a1debd213ec817ae8c230a1af
Baboyma Kagniniwa <baboyma@gmail.com>
2023-08-24 16:08:35 -0400

    CDI Draft Review - Cascade Visuals

--- Scripts/03_FY23Q3_Data_Review.R
+++ Scripts/03_FY23Q3_Data_Review.R
@@ -1,16 +1,27 @@
-# PURPOSE: SI Support for Cote d'Ivoire Programs
-# AUTHOR:  Baboyma Kagniniwa | USAID/OHA/SIEI/SI
-# PURPOSE: Global config and params
-# REF ID:  b7096822
+# PURPOSE: Munge and Analysis of
+# AUTHOR: Baboyma Kagniniwa | USAID/OHA/SIEI/SI
+# PURPOSE: FY23Q3i Data Review
+# REF ID: 8387bcfe
 # LICENSE: MIT
-# DATE:    2023-08-24
-# UPDATE:  2023-08-24
+# DATE: 2023-08-22
+# UPDATE: 2023-08-24
 # NOTES:
 
 # LOCALS & SETUP ============================================================================
 
   # Libraries
-    oha
+    library(glitr)
+    library(glamr)
+    library(gisr)
+    library(gophr)
+    library(cascade)
+    library(tidyverse)
+    library(scales)
+    library(sf)
+    library(extrafont)
+    library(tidytext)
+    library(patchwork)
+    library(ggtext)
 
   # Set paths
 
@@ -22,32 +33,45 @@
     dir_mer <- glamr::si_path("path_msd")
     dir_ras <- glamr::si_path("path_raster")
     dir_shp <- glamr::si_path("path_vector")
-    dir_cntry <- file.path("../../PEPFAR/COUNTRIES/Cote d'Ivoire")
 
   # Set Params
 
-    ref_id <- "b7096822"
+    ref_id <- "8387bcfe"
     agency <- "USAID"
     cntry <- "Cote d'Ivoire"
 
   # Files
 
     file_nat <- si_path() %>% return_latest("NAT_SUBNAT")
-    file_psnu <- si_path() %>% return_latest("PSNU_IM_FY21")
+    file_psnu <- si_path() %>% return_latest("PSNU_IM_FY21.*Cote")
+
+    file_psnu %>% get_metadata()
+
+    meta <- metadata
 
   # Functions
 
 # LOAD DATA ============================================================================
 
-  #
+  df_msd_psnu <- file_psnu %>% read_psd()
+
 
 # MUNGE ============================================================================
 
-  #
+  # Cascade Data
+  df_cascade <- df_msd_psnu %>%
+      return_cascade(cscd_num = 1)
+
+  df_cascade %>% glimpse()
 
 # VIZ ============================================================================
 
-  #
+  ## Check cascade plots
+  plot_name
+
+  return_cascade_plot(msd_df = df_msd_psnu,
+                      export = T,
+                      path = dir_images)
 
 # OUTPUTS ============================================================================
 
